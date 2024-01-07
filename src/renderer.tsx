147bbf04b5d0871c466d20639eff74f4fc06148a
Masayuki Sunahara <tamanishi@gmail.com>
2024-01-07 11:02:49 +0900

    viewport

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -12,6 +12,7 @@ export const renderer = jsxRenderer(
     return (
       <html>
         <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link href="/static/style.css" rel="stylesheet" />
           <script src="https://unpkg.com/htmx.org@1.9.10"></script>
           <title>{title}</title>
