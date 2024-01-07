42dbaa58707413e9d34c2bbc0494841e0287548c
Masayuki Sunahara <tamanishi@gmail.com>
2024-01-07 10:40:06 +0900

    remove css link

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -14,7 +14,6 @@ export const renderer = jsxRenderer(
         <head>
           <link href="/static/style.css" rel="stylesheet" />
           <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-          {/* <script src="https://cdn.tailwindcss.com"></script> */}
           <title>{title}</title>
         </head>
         <body>{children}</body>
