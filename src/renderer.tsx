da183e39e422048f34f181c8ca903b2d1225df65
Masayuki Sunahara <tamanishi@gmail.com>
2024-01-28 11:44:59 +0900

    analytics

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,26 +2,29 @@ import 'hono'
 import { jsxRenderer } from 'hono/jsx-renderer'
 
 declare module 'hono' {
-  interface ContextRenderer {
-    (content: string | Promise<string>, props?: { title?: string }): Response
-  }
+    interface ContextRenderer {
+        (content: string | Promise<string>, props?: { title?: string }): Response
+    }
 }
 
 export const renderer = jsxRenderer(
-  ({ children, title }) => {
-    return (
-      <html>
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link href="/static/style.css" rel="stylesheet" />
-          <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-          <title>{title}</title>
-        </head>
-        <body>{children}</body>
-      </html>
-    )
-  },
-  {
-    docType: true
-  }
+    ({ children, title }) => {
+        return (
+            <html>
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <link href="/static/style.css" rel="stylesheet" />
+                    <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+                    <title>{title}</title>
+                </head>
+                <body>
+                    {children}
+                    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "031832151d1e4eb98cd8a98c2399712f"}'></script>
+                </body>
+            </html>
+        )
+    },
+    {
+        docType: true
+    }
 )
